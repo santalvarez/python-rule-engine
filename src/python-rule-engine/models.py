390929efa547bf814fa4dbc40175cd430ae4b746
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2022-12-19 19:47:09 -0300

    Add rule engine models

---
+++ src/python-rule-engine/models.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from abc import ABC
+from typing import List, Any, Union
+from pydantic import BaseModel, root_validator, validator
+
+
+class Condition(BaseModel, ABC):
+    match: bool = False
+
+class SimpleCondition(Condition):
+    operator: str
+    params: dict = {}
+    value: Any
+    field: str = None
+    match_detail: Any = None
+
+    # pylint: disable=no-self-argument,no-self-use
+    @validator("field")
+    def validate_field(cls, value):
+        if value[:2] != "$.":
+            raise ValueError("Invalid Definition: First two chars of 'field' have to be $.")
+        return value
+
+class MultiCondition(Condition):
+    all: List[Union[SimpleCondition, MultiCondition]] = None
+    any: List[Union[SimpleCondition, MultiCondition]] = None
+
+    # pylint: disable=no-self-argument,no-self-use
+    @root_validator
+    def validate_one_is_present(cls, values):
+        if values['all'] and values['any']:
+            raise ValueError("Only one operator is supported")
+        if not values['all'] and not values['any']:
+            raise ValueError("No valid operators found")
+        return values
+
+class Rule(BaseModel):
+    name: str
+    conditions: MultiCondition
+
+SimpleCondition.update_forward_refs()
