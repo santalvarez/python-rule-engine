6fa34dba03f71caa4140cf5f9fe4dada060a0340
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2022-12-20 20:53:34 -0300

    Add more built-in operators

--- src/python_rule_engine/operators.py
+++ src/python_rule_engine/operators.py
@@ -29,3 +29,75 @@ class Equal(Operator):
     def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
         """ Return True if both values match"""
         return condition.value == obj_value, obj_value
+
+class NotEqual(Operator):
+    id = "not_equal"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if both values do not match"""
+        return condition.value != obj_value, obj_value
+
+class LessThan(Operator):
+    id = "less_than"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value is less than the condition value"""
+        return obj_value < condition.value, obj_value
+
+class LessThanInclusive(Operator):
+    id = "less_than_inclusive"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value is less than or equal to the condition value"""
+        return obj_value <= condition.value, obj_value
+
+class GreaterThan(Operator):
+    id = "greater_than"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value is greater than the condition value"""
+        return obj_value > condition.value, obj_value
+
+class GreaterThanInclusive(Operator):
+    id = "greater_than_inclusive"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value is greater than or equal to the condition value"""
+        return obj_value >= condition.value, obj_value
+
+class In(Operator):
+    id = "in"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value is in the condition value"""
+        return obj_value in condition.value, obj_value
+
+class NotIn(Operator):
+    id = "not_in"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value is not in the condition value"""
+        return obj_value not in condition.value, obj_value
+
+class Contains(Operator):
+    id = "contains"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value contains the condition value"""
+        return condition.value in obj_value, obj_value
+
+class NotContains(Operator):
+    id = "not_contains"
+
+    @staticmethod
+    def match(condition, obj_value, run_condition) -> Tuple[bool, Any]:
+        """ Return True if the object value does not contain the condition value"""
+        return condition.value not in obj_value, obj_value
