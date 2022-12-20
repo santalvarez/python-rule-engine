a513d2369294df22968f8faad50bd632d5d3a943
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2022-12-20 12:45:18 -0300

    Delete unused setup files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="python-rule-engine",
-    install_requires=[
-          "pydantic",
-      ],
-)
