cc855c57088c47663be949993d241be757e6bcbc
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-03-16 13:29:53 -0300

    Add JSONPathValueNotFound error

--- src/python_rule_engine/exceptions.py
+++ src/python_rule_engine/exceptions.py
@@ -7,3 +7,7 @@ class OperatorNotFoundError(RuleEngineBaseException):
 class DuplicateOperatorError(RuleEngineBaseException):
     """ Raised when there is already a operator with the same ID
     loaded in the engine """
+
+class JSONPathValueNotFound(RuleEngineBaseException):
+    """ Raised when the value indicated by 'path' could not be found
+    on the object."""
