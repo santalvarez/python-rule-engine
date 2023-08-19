51e47da0b4368ad2cca5a6ab86f3a3494c55e1f3
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-08-19 14:13:21 -0300

    Remove object tests

--- tests/test_rule_engine.py
+++ tests/test_rule_engine.py
@@ -37,39 +37,3 @@ def test_basic_rule_on_dict():
 
     assert results[0].conditions.match is True
 
-
-def test_basic_rule_on_object():
-    class Person:
-        def __init__(self):
-            self.name = "Santiago"
-            self.last_name = "Alvarez"
-
-    class Example:
-        def __init__(self):
-            self.person = Person()
-
-    obj = Example()
-
-    rule = {
-        "name": "basic_rule",
-        "conditions": {
-            "all": [
-                {
-                    "path": "$.person.name",
-                    "value": "Santiago",
-                    "operator": "equal"
-                },
-                {
-                    "path": "$.person.last_name",
-                    "value": "Alvarez",
-                    "operator": "equal"
-                }
-            ]
-        }
-    }
-
-    engine = RuleEngine([rule])
-
-    results = engine.evaluate(obj)
-
-    assert results[0].conditions.match is True
